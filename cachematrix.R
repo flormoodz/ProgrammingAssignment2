633073cc3eb6990245d8edabed586f63ea173fb0
flormoodz <71810404+flormoodz@users.noreply.github.com>
2020-10-02 11:08:07 +0800

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,46 @@
-## Put comments here that give an overall description of what your
-## functions do
+##Programming Assignment 2: Lexical Scoping
 
-## Write a short comment describing this function
+## This R file contains a pair of functions that cache the inverse
+## of a matrix.
+
+## The first function below creates a special "matrix" object that 
+## can cache its inverse.
 
 makeCacheMatrix <- function(x = matrix()) {
-
+	inverse <- NULL
+	
+	##set the value of the matrix
+	set <- function(y) {
+                x <<- y
+                inverse <<- NULL
+      }
+	
+	##get the value of the matrix
+      get <- function() x
+	
+	##set the value of the inverse of the matrix
+      setinverse <- function(inv) inverse <<- inv	
+	
+	##get the value of the inverse of the matrix
+	getinverse <- function() inverse
+	list(set = set, get = get,
+		setinverse = setinverse,
+            getinverse = getinverse)
 }
 
 
-## Write a short comment describing this function
+## The second function below computes the inverse of the special 
+## "matrix" returned by the first function.
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+        inverse <- x$getinverse()
+        if(!is.null(inverse)) {
+                message("getting cached data")
+                return(inverse)
+        }
+        data <- x$get()
+        inverse <- solve(data, ...)
+        x$setinverse(inverse)
+        inverse
 }
